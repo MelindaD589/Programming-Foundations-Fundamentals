e6c77de4b0773b83a100558a9670a5c8dc2c574a
Melinda Dothan <melinda.dothan@gmail.com>
2021-03-08 09:43:01 +0100

    Updating practice 2

--- practice 2.py
+++ practice 2.py
@@ -1,2 +1,30 @@
 # Chapter 3
 # Exercise 1
+
+age = 36
+print(age)
+print(type(age))
+
+email_address = "john.doe@me.com"
+print(email_address)
+print(type(email_address))
+
+# Exercise 2
+
+cookies = 'Sugar'
+print(cookies)
+print(type(cookies))
+
+cookies = 1
+print(cookies)
+print(type(cookies))
+
+Cookies = 3
+print(Cookies)
+print(cookies)
+
+first_name = "Jeff"
+print(first_name)
+
+first_Name = "Sara"
+print(first_Name)
