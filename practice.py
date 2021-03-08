b35965a173a3a8e680fed21563f17099c077b7ab
Melinda Dothan <melinda.dothan@gmail.com>
2021-03-08 09:37:58 +0100

    Chapter 1 and 2

---
+++ practice.py
@@ -0,0 +1,34 @@
+# Chapter 1
+
+print("Hello world!")
+
+# Chapter 2
+# Exercise 1
+
+name = input("Hi, what's your name? ")
+age = int(input("How old are you? "))
+
+if (age < 13):
+    print("You're too young to register", name)
+else:
+    print("Feel free to join", name)    
+
+# Exercise 2
+
+print("Hello world!")
+
+print("Goodbye world!")
+
+# Exercise 3
+
+# syntax error
+print("Hello world")
+
+# runtime error
+10 * (2/0)
+
+# semantic error
+name = "Alice"
+print("Hello name")
+
+
