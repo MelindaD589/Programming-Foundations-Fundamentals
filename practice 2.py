362803451441b61cff9ec968b91f9667ddca482b
Melinda Dothan <melinda.dothan@gmail.com>
2021-03-22 12:03:15 +0100

    Updating practice 2

--- practice 2.py
+++ practice 2.py
@@ -28,3 +28,15 @@ print(first_name)
 
 first_Name = "Sara"
 print(first_Name)
+
+
+# Exercise 3
+
+print("Hi!")
+name = input("What's your name? ")
+print("It's nice to meet you,", name)
+answer = input("Are you enjoying the course?")
+if answer == "Yes":
+    print("That's good to hear!")
+else:
+    print("Oh no! That makes me sad!")
